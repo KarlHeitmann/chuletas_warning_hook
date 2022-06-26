80611505ff3a2921308ebcb244a7c38dd87fc251
Karl Heitmann <khf1988@gmail.com>
2022-06-25 23:45:45 -0400

    spawns git diff for staged files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::process::{Command, Stdio};
+use std::str;
+
 fn main() {
-    println!("Hello, world!");
+    let child = Command::new("git")
+        .args("diff --cached --name-only --diff-filter=ACM".split(' '))
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("failed to execute child");
+    let output = child
+        .wait_with_output()
+        .expect("failed to wait on child");
+
+    assert!(output.status.success());
+    //println!("{:?}", output.stdout);
+    let s = match str::from_utf8(&output.stdout) {
+        Ok(v) => v,
+        Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+    };
+    println!("result: {}", s);
 }
